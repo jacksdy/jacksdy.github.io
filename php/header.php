09f5a3898dbb47c8262e8a52f45a589e37bf0972
Asdrubal Gonzalez <asdrubalgg@gmail.com>
2018-10-31 22:09:13 -0400

    ok inicio

---
+++ php/header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>.::Blog - JackWebDesing::.</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/estilos2.css">
+	<!-- Este  -->
+</head>
+<body>
+	<header>
+		<nav class="navbar navbar-inverse navbar-static-top" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navegacion-ajgg">
+						<span class="sr-only">Desplegar / Ocultar Menú</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a href="Loggin.html" class="navbar-brand">JackWebDesing</a>
+				</div>
+				<!-- Menu -->
+				<div class="collapse navbar-collapse" id="navegacion-ajgg">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#">Inicio</a></li>
+						<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Categorias <span class="caret"></span></a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="#">Categoria #1</a></li>
+								<li class="divider"></li>
+								<li><a href="#">Categoria #2</a></li>
+								<li><a href="#">Categoria #3</a></li>
+								<li><a href="#">Categoria #4</a></li>
+							</ul>							
+						</li>
+						<li><a href="Blogg.html">Blogg</a></li>
+						<li><a href="Contacto.html">Contacto</a></li>
+					</ul>
+					<form action="" class="navbar-form navbar-right" role="search">
+						<div class="form group">
+							<input type="text" class="form-control" placeholder="Buscar">
+							<button type="submit" class="btn btn-primary">
+							<span class="glyphicon glyphicon-search"></span>
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>	
+		</nav>
+	</header>
---
+++ php/index.php
@@ -0,0 +1,155 @@
+<?php include ("header.php") ?>
+
+	<!-- Jumbotron -->
+	<section class="jumbotron">
+		<div class="container">
+
+		<!-- Creado por Asdrúbal -->
+			<div class="jumbo-one">
+				<h1>JackWebDesing</h1>
+				<p>Blog de diseño web</p>
+			</div>
+			<div class="jumbo-two">
+				<img class="jumbo2img" src="imagenes/jack2.png" alt="Logo JackWebDesing"> 
+			</div>
+		 <!-- creado por Asdrúbal	 imagenes/JWD_2016.png-->
+
+		</div>
+	</section>
+
+	<section class="main container">
+		<div class="row">
+			<section class="posts col-md-9">
+				<div class="miga-de-pan">
+					<ol class="breadcrumb">
+						<li><a href="">Inicio</a></li>
+						<li><a href="">Categoria</a></li>
+						<li><a href="">Diseño Web</a></li>
+					</ol>
+				</div>
+				<article class="post clearfix">
+					<a href="#" class="thumb pull-left">
+						<img class="img-thumbnail" src="imagenes/100Asdy.png" alt="Imagen01">
+						</a>
+						<h2 class="post-title">
+							<a href="#">Inicia proyectos HTML5</a>
+						</h2>
+						<p><span class="post-fecha">26 de Junio de 2016</span> por <span class="post-autor"><a href="#">Asdrúbal González</a></span></p>
+					<p class="post-contenido text-justify">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta officiis ex culpa ducimus accusamus sed distinctio, voluptatem tempora alias debitis adipisci doloremque pariatur impedit quae perspiciatis eveniet est quasi quis.
+					</p>
+					<div class="contenedor-botones">
+						<a href="#" class="btn btn-primary">Leer Mas</a>
+						<a href="#" class="btn btn-success">Comentarios <span class="badge">20</span></a>
+					</div>
+				</article>
+								<article class="post clearfix">
+					<a href="#" class="thumb pull-left">
+						<img class="img-thumbnail" src="imagenes/pienso.jpg" alt="Imagen01">
+						</a>
+						<h2 class="post-title">
+							<a href="#">Inicia proyectos HTML5</a>
+						</h2>
+						<p><span class="post-fecha">26 de Junio de 2016</span> por <span class="post-autor"><a href="#">Asdrúbal González</a></span></p>
+					<p class="post-contenido text-justify">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta officiis ex culpa ducimus accusamus sed distinctio, voluptatem tempora alias debitis adipisci doloremque pariatur impedit quae perspiciatis eveniet est quasi quis.
+					</p>
+					<div class="contenedor-botones">
+						<a href="#" class="btn btn-primary">Leer Mas</a>
+						<a href="#" class="btn btn-success">Comentarios <span class="badge">20</span></a>
+					</div>
+				</article>
+								<article class="post clearfix">
+					<a href="#" class="thumb pull-left">
+						<img class="img-thumbnail" src="imagenes/leer.jpg" alt="Imagen01">
+						</a>
+						<h2 class="post-title">
+							<a href="#">Inicia proyectos HTML5</a>
+						</h2>
+						<p><span class="post-fecha">26 de Junio de 2016</span> por <span class="post-autor"><a href="#">Asdrúbal González</a></span></p>
+					<p class="post-contenido text-justify">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta officiis ex culpa ducimus accusamus sed distinctio, voluptatem tempora alias debitis adipisci doloremque pariatur impedit quae perspiciatis eveniet est quasi quis.
+					</p>
+					<div class="contenedor-botones">
+						<a href="#" class="btn btn-primary">Leer Mas</a>
+						<a href="#" class="btn btn-success">Comentarios <span class="badge">20</span></a>
+					</div>
+				</article>
+
+				<nav>
+					<div class="center-block">
+						<ul class="pagination">
+						<li class="disabled"><a href="#">&laquo; <span class="sr-only">Anterior</span></a></li>
+						<li class="active"><a href="#">1</a></li>
+						<li><a href="#">2</a></li>
+						<li><a href="#">3</a></li>
+						<li><a href="#">4</a></li>
+						<li><a href="#">5</a></li>
+						<li><a href="#">&raquo; <span class="sr-only">Siguiente</span></a></li>
+						</ul>
+					</div>
+
+				</nav>
+			</section>
+
+	<aside class="col-md-3 hidden-xs hidden-sm">
+		<h4>Categorias</h4>
+		<div class="list-group">
+			<a href="#" class="list-group-item active">Diseño Web</a>
+			<a href="#" class="list-group-item">CSS</a>
+			<a href="#" class="list-group-item">Cursos</a>
+			<a href="#" class="list-group-item">Desarrollo Web</a>
+			<a href="#" class="list-group-item">Elementos Web</a>
+			<a href="#" class="list-group-item">Jquery</a>
+			<a href="#" class="list-group-item">Recursos y Herramientas</a>
+		</div>
+
+		<h4>Articulos Recientes</h4>
+		<a href="#" class="list-group-item">
+			<h4 class="list-group-item-heading">Inicia proyectos HTML 5 más rápidos</h4>
+			<p class="list-group--item-text">Aprende un poco mas de cada una de las nuevas tendencias en programación</p>
+		</a>
+				<a href="#" class="list-group-item">
+			<h4 class="list-group-item-heading">Inicia proyectos HTML 5 más rápidos</h4>
+			<p class="list-group--item-text">Aprende un poco mas de cada una de las nuevas tendencias en programación</p>
+		</a>
+				<a href="#" class="list-group-item">
+			<h4 class="list-group-item-heading">Inicia proyectos HTML 5 más rápidos</h4>
+			<p class="list-group--item-text">Aprende un poco mas de cada una de las nuevas tendencias en programación</p>
+		</a>
+	</aside>
+
+		</div>
+	</section>
+
+	
+
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-6">
+					<p>Asdrúbal González - JackWebDesing</p>
+				</div>
+				<div class="col-xs-6">
+					<ul class="list-inline text-right">
+						<li><a href="#">Inicio</a></li>
+						<li><a href="#">Cursos</a></li>
+						<li><a href="#">Contacto</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+
+
+<!-- 	<script src="js/jquery.js"></script> -->
+
+
+<script
+        src="https://code.jquery.com/jquery-1.12.4.min.js"
+        integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="
+        crossorigin="anonymous"></script>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
